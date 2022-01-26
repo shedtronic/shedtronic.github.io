f8de772a3faaa4558226f76dcafaa0308b334c5f
Mike Ballard <MBallard@cityofbristol.ac.uk>
2022-01-26 21:21:11 +0000

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -8,6 +8,12 @@ frameRate(30);
 
 function draw() {
   // put drawing code here
+  background(0); // Set the background to black
+  y = y - 1;
+  if (y < 0) {
+    y = height;
+  }
+  line(0, y, width, y);
 }
 
 
