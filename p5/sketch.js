eecf231e7af5be8cbf4a0a7728e7ff213cc57e5f
Mike Ballard <MBallard@cityofbristol.ac.uk>
2022-01-26 21:20:26 +0000

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
   // put setup code here
+
+  createCanvas(720, 400);
+stroke(255); // Set line drawing color to white
+frameRate(30);
 }
 
 function draw() {
   // put drawing code here
 }
+
+
